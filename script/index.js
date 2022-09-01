34972a641aa7a4440932cabdfbde678afa226a5e
urmat4002 <urmat4002@gmail.com>
2022-09-01 20:13:33 +0600

    add description of ingredient

--- script/index.js
+++ script/index.js
@@ -9,7 +9,7 @@ const GET_ALL_COCKTAILS = API + 'filter.php?c=Cocktail'
 const GET_BY_NAME = API + 'search.php?s='
 const GET_FYLTER = API + 'filter.php?a='
 const GET_INGREDIENTS = API + 'lookup.php?i='
-const GET_THE_INGREDIENT = API + 'filter.php?i='
+const GET_THE_INGREDIENT = API + 'search.php?i='
 
 const getAllCocktails = async () => {
   const request = await fetch(GET_ALL_COCKTAILS)
@@ -48,36 +48,46 @@ const getSelected = async (item) => {
 const getIngredient = async (ingredient) => {
   const request = await fetch(GET_THE_INGREDIENT + ingredient)
   const response = await request.json()
-  //console.log(response)
-  renderCocktails(response.drinks, false)
+  // console.log(response.ingredients[0])
+
+  renderIngDescription(response.ingredients[0])
+}
+
+const renderIngDescription = (data) => {
+  output.innerHTML = ''
+  const img = document.createElement('img')
+
+  const cardDesc = document.createElement('div')
+  const title = document.createElement('h2')
+  title.textContent = data.strIngredient
+  cardDesc.className = 'cardDesc'
+  const desc = document.createElement('p')
+  img.src = `https://thecocktaildb.com/images/ingredients/${title.textContent}-Small.png`
+
+  desc.textContent =
+    data.strDescription == null ? 'Описание отсутствует' : data.strDescription
+
+  cardDesc.append(title, img, desc)
+  output.append(cardDesc)
+
+  //console.log(data.strDescription)
 }
 
 const renderCocktails = (drinks, displayIng) => {
   // console.log(drinks)
   output.innerHTML = ''
+  //console.log(drinks)
   drinks
     ? drinks.map((el) => {
         const card = document.createElement('div')
         const img = document.createElement('img')
         const title = document.createElement('h2')
         const ingredients = document.createElement('div')
+        const cockDesc = document.createElement('p')
+
+        // console.log(el)
 
         if (displayIng) {
-          /*const ingredient1 = document.createElement('p')
-          const ingredient2 = document.createElement('p')
-          const ingredient3 = document.createElement('p')
-          const ingredient4 = document.createElement('p')
-
-          ingredient1.textContent = el.strIngredient1
-          ingredient2.textContent = el.strIngredient2
-          ingredient3.textContent = el.strIngredient3
-          ingredient4.textContent = el.strIngredient4
-
-          ingredient1.addEventListener('click', () => {
-            getIngredient(el.strIngredient1)
-
-          })*/
-
           ingredients.innerHTML = '<h4>Ingredients:</h4>'
           for (let i = 0; i < 4; i++) {
             const ingredient = document.createElement('p')
@@ -92,11 +102,9 @@ const renderCocktails = (drinks, displayIng) => {
               getIngredient(ingredient.textContent)
             })
           }
-
-          //ingredients.append(ingredient1, ingredient2, ingredient3, ingredient4)
-          //console.log(el.strGlass)
         }
 
+        cockDesc.textContent = el.strIBA
         img.src = el.strDrinkThumb
         img.style.width = '200px'
         img.style.height = '200px'
@@ -108,7 +116,7 @@ const renderCocktails = (drinks, displayIng) => {
           getSelected(el)
         })
 
-        card.append(img, title, ingredients)
+        card.append(img, title, cockDesc, ingredients)
         output.append(card)
       })
     : (output.innerHTML = `<h1>Server error</h1>`)
